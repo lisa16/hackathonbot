ff97cd47715f5af3621dc5580b9891c3866f9b0d
Lisa Wong <miss.lisa19@hotmail.com>
2016-06-01 13:55:32 -0700

    echo bot

---
+++ server.js
@@ -0,0 +1,27 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: 'mshackathonbot', 
+    appSecret: 'a5a380f8bafd486da521906d4cce596d' 
+};
+
+// Create bot
+var bot = new builder.BotConnectorBot(botConnectorOptions);
+bot.add('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
